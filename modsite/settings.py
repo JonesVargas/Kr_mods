94f46a30d97db9644ca955220801004724fc05fd
JonesVargas <jonesvargas1980@gmail.com>
2025-02-25 12:15:00 -0300

    Configurando arquivos de mídia diretamente no Heroku

--- modsite/settings.py
+++ modsite/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -98,15 +99,15 @@ USE_I18N = True
 USE_TZ = True
 
 
-
+# Configuração de arquivos estáticos (já deve estar no seu código)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Configuração de mídia
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Configuração de arquivos estáticos (já deve estar no seu código)
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
