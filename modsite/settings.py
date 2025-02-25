1a0ffd56b7358b0fbb17b57c23a42bce6e70e6f0
JonesVargas <jonesvargas1980@gmail.com>
2025-02-25 13:52:13 -0300

    Corrigindo configuração do Cloudinary e do banco de dados

--- modsite/settings.py
+++ modsite/settings.py
@@ -1,29 +1,25 @@
 import os
 from pathlib import Path
-import cloudinary  # Correto
+import dj_database_url
+import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+from dotenv import load_dotenv
+load_dotenv()
+
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Diretório base do projeto
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3@f!7%ad6(07yk67&ossrgantu@l)g#7lv-b3oo%qzlj8q6v7r'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# Segurança
+SECRET_KEY = os.getenv('SECRET_KEY', 'chave-secreta-de-desenvolvimento')
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['127.0.0.1', 'kr-mods-d517b12a9a57.herokuapp.com', 'krsoftwares.com.br']
 
-
-# Application definition
-
+# Aplicações instaladas
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -31,14 +27,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mods',  # Adicionando nosso app mods
-    'cloudinary_storage',
-    'cloudinary',
+    'mods',  # Seu app principal
+    'cloudinary_storage',  # Armazenamento de mídia no Cloudinary
+    'cloudinary',  # Biblioteca Cloudinary
 ]
 
+# Middleware
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Para servir arquivos estáticos no Heroku
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -47,8 +44,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+# Configuração do Django Templates
 ROOT_URLCONF = 'modsite.urls'
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -60,8 +57,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'mods.context_processors.get_logo',  # Adicionamos o Context Processor
-                'mods.context_processors.redes_sociais',  # Adicionando nosso contexto
+                'mods.context_processors.get_logo',  # Context Processor customizado
+                'mods.context_processors.redes_sociais',  # Outro contexto customizado
             ],
         },
     },
@@ -69,84 +66,56 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'modsite.wsgi.application'
 
-
-
+# Banco de Dados - Padrão Heroku PostgreSQL
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # Corrigido para evitar o erro
-    }
+    'default': dj_database_url.config(conn_max_age=600, conn_health_checks=True)
 }
 
-import dj_database_url  
-
-DATABASES['default'] = dj_database_url.config(
-    conn_max_age=600,
-    conn_health_checks=True,
-)
-
-
+# Validações de senha
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
-
+# Internacionalização
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
-
-# Configuração de arquivos estáticos (já deve estar no seu código)
+# Configuração de arquivos estáticos no Heroku
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-# Configuração de mídia
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# Configuração de armazenamento no Cloudinary
+cloudinary.config(
+    cloud_name=os.getenv('CLOUDINARY_CLOUD_NAME', 'dvd7rkwyu'),
+    api_key=os.getenv('CLOUDINARY_API_KEY', '416561564897632'),
+    api_secret=os.getenv('CLOUDINARY_API_SECRET', 'xOlGgthqR_2IqdhFnnI54WtJYt8'),
+    secure=True
+)
 
+# Armazenamento de arquivos de mídia no Cloudinary
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+MEDIA_URL = '/media/'  # Mantemos para referência, mas não é necessário para Cloudinary
 
-
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
+# Configuração de login e sessão
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_URL = "/login/"
-
-
-
 SESSION_ENGINE = "django.contrib.sessions.backends.db"
 SESSION_EXPIRE_AT_BROWSER_CLOSE = False
-SESSION_COOKIE_AGE = 3600  # Sessão dura 1 hora
-# Configuração para envio de e-mails (Gmail)
+SESSION_COOKIE_AGE = 3600  # Sessão expira após 1 hora
+
+# Configuração de email (Gmail)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'jnsvirtual1311@gmail.com'  # Substitua pelo seu e-mail
-EMAIL_HOST_PASSWORD = 'kpyp lwss rlkr ejqg'  # Substitua pela senha do seu e-mail
+EMAIL_HOST_USER = 'jnsvirtual1311@gmail.com'
+EMAIL_HOST_PASSWORD = 'kpyp lwss rlkr ejqg'  # 🚨 **Não exponha credenciais no código! Use variáveis de ambiente**
 
-cloudinary.config( 
-    cloud_name = "dvd7rkwyu", 
-    api_key = "416561564897632", 
-    api_secret = "xOlGgthqR_2IqdhFnnI54WtJYt8", # Click 'View API Keys' above to copy your API secret
-    secure=True
-)
-
-DEFAULT_FILE_STORAGE  =  'cloudinary_storage.storage.MediaCloudinaryStorage'
+# Configuração padrão para novos modelos do Django
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
