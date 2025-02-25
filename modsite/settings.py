5f8e059eb2d7ccea0185084e1e32ca055ba56326
JonesVargas <jonesvargas1980@gmail.com>
2025-02-25 12:47:48 -0300

    Configuração do novo banco de dados

--- modsite/settings.py
+++ modsite/settings.py
@@ -73,6 +73,13 @@ DATABASES = {
     }
 }
 
+import dj_database_url  
+
+DATABASES['default'] = dj_database_url.config(
+    conn_max_age=600,
+    conn_health_checks=True,
+)
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
